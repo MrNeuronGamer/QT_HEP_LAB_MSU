2d91b631a75bd0061a1941127a0d332134d8a50b
MrNeuronGamer <khoroshilov.dmitry@mail.ru>
2019-09-12 21:44:42 +0300

    Neuman sort has been implemented

--- numerical_labs/task_1.9/main.cpp
+++ numerical_labs/task_1.9/main.cpp
@@ -8,7 +8,7 @@ typedef unsigned int ui;
 const ui R = UINT_MAX;
 ui A = 999;
 ui B = 9999;
-size_t N = 33000000;
+const size_t N = 33000000;
 
 void generate_ab(ui& a, ui& b)
 {
@@ -22,7 +22,7 @@ void generate_x(double& x_1, double& x_2)
 
     double s = 2;
     double u, v;
-    
+
     while (s > 1)
     {
         generate_ab(A, B);
@@ -36,7 +36,7 @@ void generate_x(double& x_1, double& x_2)
     x_1 = u*sqrt(-2.0 / s*log(s));
     x_2 = v*sqrt(-2.0 / s*log(s));
 
-    
+
 }
 
 void fill_x(double* x)
@@ -51,16 +51,98 @@ void fill_x(double* x)
 
 }
 
-void heapsort(double* arr)
+void direct_ins_sort(double* x, size_t Size)
 {
+    
+
+    size_t min_ind = 0;
+    for (size_t i = 0; i < Size; i++)
+    {
+        min_ind = i;
+
+        for (size_t j = i ; j < Size; j++)
+            if (x[j] < x[min_ind]) min_ind = j;
+
+        swap(x[i], x[min_ind]);
+
+
+    }
+
+    
+
+}
+
+void heapsort(double* arr, size_t Size)
+{
+
 
 
 }
 
+void merge(double* a, size_t as, double* b, size_t bs)
+{
+    size_t rs = as + bs;
+    double* res = new double[rs];
+    size_t i = 0;
+    size_t j = 0;
 
-void Neuman_sort(double* x)
+    while ((i < as) && (j < bs))
+    {
+
+        if (a[i] < b[j])
+        {
+            res[i + j] = a[i];
+            i++;
+
+        }
+        else
+        {
+            res[i + j] = b[j];
+            j++;
+        }
+
+      
+
+   }
+
+
+    if (i == as)
+    {
+        for (; j < bs; j++)
+            res[i + j] = b[j];
+    }
+    else
+    {
+        for (; i < as; i++)
+            res[i + j] = a[i];
+    }
+
+    for (i = 0; i < rs; i++)
+    {
+        a[i] = res[i];
+    }
+
+
+    delete[] res;
+    
+
+
+}
+
+void Neuman_sort(double* x, size_t Size)
 {
 
+
+    if (Size > 8)
+    {
+
+        Neuman_sort(x, Size / 2);
+        Neuman_sort(x + Size / 2, Size - Size / 2);
+
+        merge(x, Size / 2, x + Size / 2, Size - Size / 2);
+
+    }
+    else direct_ins_sort(x, Size);
 
 
 
@@ -68,22 +150,8 @@ void Neuman_sort(double* x)
 
 int main()
 {
-      
-
-    double* x = NULL;
-
-    x = new double [N];
-
-    fill_x(x);
-
-
 
     
-
-
-   
-    delete[] x;
-
 
     return 0;
 }
