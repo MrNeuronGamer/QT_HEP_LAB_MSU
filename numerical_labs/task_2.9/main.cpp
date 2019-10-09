91d9d5bd05ae8305b17e9517350ef46a7cbdc206
MrNeuronGamer <khoroshilov.dmitry@mail.ru>
2019-10-09 15:41:11 +0300

    Primitive solver has been implemented (uses triangulation)

--- numerical_labs/task_2.9/main.cpp
+++ numerical_labs/task_2.9/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
@@ -21,21 +22,31 @@ public:
 
     friend ostream& operator<<(ostream&, const LE_System&);
 
+
+    // debug section:
+    void printTables() const;           // prints lhs and rhs 
+    LE_System(double* Lines, size_t size);
+
 private:
     void    SwapColumn      (size_t start_pos, size_t target_pos);      // swaps columns indexed by start_pos and target_pos
     void    SwapLine        (size_t start_pos, size_t target_pos);      // swaps lines indexed by start_pos and target_pos
-    double  GetNorm         (size_t line);                              // determines a L1 norm for a line and returns it
+    double  GetNorm         (size_t line) const;                        // determines a L1 norm for a line and returns it
     void    MultiplyLine    (size_t line, double coeff);                // multiplyes a line with a coeff
-    bool    CheckResult     (double error = 1);                         // checks whether a solution found recreates a column b with a residual less then "error" lines-wise / default error = 1
+    bool    CheckResult     (double error = 1) const;                   // checks whether a solution found recreates a column b with a residual less then "error" lines-wise / default error = 1 / returns true if solutions is ok
     void    SubtractLines   (size_t from, size_t what);                 // subtracts line "what" from line "from"
     void    SettleLine      (size_t line);                              // makes shure that any line under "line" gets subtracted with "line" and gets rid of a variable indexed as "line"
-
+    void    ReduceBigNorms  ();                                         // makes sure that no line's norm is 10^2 times bigger comparing with average norm 
 };
 
 
 
 int main()
 {
+    double arr[] = { 12,0,0,3,8,7,0,1 };
+
+    LE_System A(arr, 2);
+    A.printTables();
+    A.FindSolution();
 
 
 
@@ -62,7 +73,7 @@ void LE_System::SwapLine(size_t start_pos, size_t target_pos)
 
 }
 
-double LE_System::GetNorm(size_t line)
+double LE_System::GetNorm(size_t line) const 
 {
     double norm = 0;
     for (size_t i = 0; i< Size; i++)
@@ -101,6 +112,7 @@ void LE_System::SubtractLines(size_t from, size_t what)
 
 void LE_System::SettleLine(size_t line)
 {
+    MultiplyLine(line, 1 / lhs_matrix[line + Size*line]);
     for (size_t l = line + 1; l < Size; l++)
     {
         MultiplyLine(l, 1/lhs_matrix[line+Size*l]);
@@ -112,7 +124,7 @@ void LE_System::SettleLine(size_t line)
 void LE_System::operator= (const LE_System& A)
 {
 
-    Size = A.Size;
+    Size = A.Size;  
     for (size_t i = 0; i < Size; i++)
     {
         for (size_t j = 0; j < Size; j++)
@@ -124,7 +136,89 @@ void LE_System::operator= (const LE_System& A)
 
 }
 
+void LE_System::ReduceBigNorms()
+{
+    bool flag = true;
+    while (flag)
+    {
+        flag = false;
+        double AvNorm = 0;
+        vector<double> Norms;
+        Norms.resize(Size);
 
+        for (size_t i = 0; i < Size; i++)
+        {
+            Norms[i] = GetNorm(i);
+            AvNorm += Norms[i];
+        }
+
+        AvNorm = AvNorm / Size;
+
+        for (size_t i = 0; i < Size; i++)
+        {
+            if (Norms[i] > AvNorm * 100)
+            {
+                MultiplyLine(i, Norms[i] / AvNorm);
+                flag = true;
+            }
+        }
+
+
+    }
+
+}
+
+
+
+void LE_System::FindSolution()
+{
+
+    ReduceBigNorms();           // get rid of big norms
+    
+    for (size_t i = 0; i < Size; i++)
+    {
+        SettleLine(i);           // triangulate
+    }
+
+    printTables();              //check matrix view
+
+    for (size_t i = Size - 1; Size - i != Size+1; i--)
+    {
+        solution[i] = rhs[i];
+        for (size_t j = i + 1; j < Size; j++)
+        {
+            solution[i] -= solution[j] * lhs_matrix[i*Size+j];
+        }
+    }
+
+   
+
+    if (!CheckResult())
+        cout << "\nSmth gone wrong\n\n\n\n";
+    else cout << "\n\n All's fine Dude! ;) \n";
+
+    printTables();
+
+
+
+}
+
+bool LE_System::CheckResult(double error ) const
+{
+
+    for (size_t i = 0; i < Size; i++)
+    {
+        double LHS = 0;
+        for (size_t j = 0; j < Size; j++)
+        {
+            LHS += solution[j] * lhs_matrix[i*Size + j];
+        }
+
+        if (abs(LHS - rhs[i]) > error) return false;
+    }
+
+    return true;
+}
 
 
 LE_System::~LE_System()
@@ -133,4 +227,50 @@ LE_System::~LE_System()
     delete[] rhs;
     delete[] solution;
 
+}
+
+
+
+
+
+LE_System::LE_System(double* arr, size_t size)
+{
+
+    lhs_matrix = new double[size*size];
+    solution = new double[size];
+    rhs = new double[size];
+    Size = size;
+
+    for (size_t i = 0; i < Size; i++)
+    {
+
+        for (size_t j = 0; j < Size; j++)
+        {
+            lhs_matrix[i*Size + j] = arr[i*(Size + 2) + j];
+
+        }
+        solution[i] = arr[i*(Size + 2) + Size];
+        rhs[i] = arr[i*(Size + 2) + Size+1];
+
+    }
+
+
+}
+
+
+
+void LE_System::printTables() const
+{
+
+    for (size_t i = 0; i < Size; i++)
+    {
+        for (size_t j = 0; j < Size; j++)
+            cout << lhs_matrix[i*Size + j] << " ";
+
+        cout << "  ::  " << solution[i] << "  ::  " << rhs[i] << "\n";
+    }
+
+    int c = 0;
+    cin >> c;
+    cout << "\n\n\n";
 }
